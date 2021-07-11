64410d0758e4a237c257d37ca111b41f8b7b0559
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-11 13:02:40 -0600

    Switched BITS on leds...match MSP430

--- project/blink2/led.h
+++ project/blink2/led.h
@@ -1,9 +1,11 @@
 #ifndef led_included
 #define led_included
 
-#define LED_RED BIT6 //changed              // P1.0
-#define LED_GREEN BIT0        //flip back     // P1.6
+#define LED_RED BIT6  //msp430 P1.6
+#define LED_GREEN BIT0  //MSP430 P1.0
+
 #define LEDS (BIT0 | BIT6)
+
 extern unsigned char red_on, green_on;
 extern unsigned char led_changed;
 
