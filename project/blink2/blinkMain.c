50642b7842baea1c60cacfa5748c9b9d906d32da
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-11 13:01:17 -0600

    Update

--- project/blink2/blinkMain.c
+++ project/blink2/blinkMain.c
@@ -2,9 +2,10 @@
 #include <msp430.h>
 #include "libTimer.h"
 #include "led.h"  
+
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  led_init(); //led init 
+  led_init(); //enable led??
   enableWDTInterrupts();	/* enable periodic interrupt */
 
   or_sr(0x18);		/* CPU off, GIE on */
