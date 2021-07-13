ceb802da7daeaae9a4c58d4889f397849dcd2467
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-12 22:53:19 -0600

    Beta test for buzzer and led dims

--- project/blink2/wdInterruptHandler.c
+++ project/blink2/wdInterruptHandler.c
@@ -2,10 +2,15 @@
 #include "stateMachines.h"
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
-  static char blink_count = 0;
-  if (++blink_count == 125){
-    turn_red();
-    turn_green();
-    blink_count = 0;
-  }
+  //static char blink_count = 0;
+  //if (++blink_count == 125){
+    //turn_red();
+    //turn_green();
+    //sound_on();
+    //blink_count = 0;
+  //}
+
+  turn_red();
+  turn_green();
+  sound_on();
 }
