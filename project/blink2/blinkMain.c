516b66656712471e9b85289c21172ce5e4d27807
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-12 22:51:38 -0600

    modifications

--- project/blink2/blinkMain.c
+++ project/blink2/blinkMain.c
@@ -3,11 +3,15 @@
 #include "../timerLib/libTimer.h"
 #include "led.h"  
 #include "switches.h"
+#include "buzzer.h"
 
 int main(void) {
+
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
-  led_init(); //enable led??
+  led_init(); 
   switch_init();
+  buzzer_init();
+  buzzer_set_period(0);
   enableWDTInterrupts();	/* enable periodic interrupt */
   
   or_sr(0x18);		/* CPU off, GIE on */
