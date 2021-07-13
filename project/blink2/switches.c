c5243d0890861373e4ba39d7af596da606fdd8d2
oanevarez <42950984+oanevarez@users.noreply.github.com>
2021-07-12 18:52:30 -0600

    Update switches.c

--- project/blink2/switches.c
+++ project/blink2/switches.c
@@ -2,34 +2,35 @@
 #include "switches.h"
 #include "led.h"
 
-char switch_state_down, switch_state_changed; /* effectively boolean */
+char switch_state_down_1, switch_state_changed; /* effectively boolean */
+char switch_state_down_2; /* effectively boolean */
+char switch_state_down_3; /* effectively boolean */
+char switch_state_down_4; /* effectively boolean */
 
-static char 
-switch_update_interrupt_sense()
+static char switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
+  P2IES |= (p2val & SWITCHES);/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);/* if switch down, sense up */
+  return p2val;
 }
 
-void 
-switch_init()			/* setup switch */
-{  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
-  switch_update_interrupt_sense();
-  led_update();
-}
-
-void
-switch_interrupt_handler()
+void switch_init()/* setup switch */
 {
-  char p1val = switch_update_interrupt_sense();
-  switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
-  switch_state_changed = 1;
-  led_update();
+  P2REN |= SWITCHES;/* enables resistors for switches */
+  P2IE |= SWITCHES;/* enable interrupts from switches */
+  P2OUT |= SWITCHES;/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;/* set switches' bits for input */
+  switch_update_interrupt_sense();
+}
+
+void switch_interrupt_handler()
+{  
+  char p2val = switch_update_interrupt_sense();
+  switch_state_down_1 = (p2val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+  switch_state_down_2 = (p2val & SW2) ? 0 : 1; /* 0 when SW2 is up */
+  switch_state_down_3 = (p2val & SW3) ? 0 : 1; /* 0 when SW3 is up */
+  switch_state_down_4 = (p2val & SW4) ? 0 : 1; /* 0 when SW4 is up */
+  switch_state_changed = 1; 
 }
