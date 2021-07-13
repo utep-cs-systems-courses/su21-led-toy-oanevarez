920141d539d890527adf9309e792e599939f6883
oanevarez <42950984+oanevarez@users.noreply.github.com>
2021-07-12 18:53:28 -0600

    Update switches.h

--- project/blink2/switches.h
+++ project/blink2/switches.h
@@ -1,12 +1,20 @@
+
 #ifndef switches_included
 #define switches_included
 
-#define SW1 BIT3		/* switch1 is p1.3 */
-#define SWITCHES SW1		/* only 1 switch on this board */
+#define SW1 BIT0
+#define SW2 BIT1
+#define SW3 BIT2
+#define SW4 BIT3
+#define SWITCHES (SW1|SW2|SW3|SW4)
 
 void switch_init();
 void switch_interrupt_handler();
 
-extern char switch_state_down, switch_state_changed; /* effectively boolean */
+extern char interruptTime;
+extern char switch_state_down_1, switch_state_changed; /* effectively boolean */
+extern char switch_state_down_2; /* effectively boolean */
+extern char switch_state_down_3; /* effectively boolean */
+extern char switch_state_down_4; /* effectively boolean */
 
 #endif // included
