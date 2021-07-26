cae05ea3d491337a170aef03fb74d7f89c916352
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-26 17:04:20 -0600

    Will update soon to match modern interrupt

--- project/toy/wdInterruptHandler.c
+++ project/toy/wdInterruptHandler.c
@@ -3,14 +3,16 @@
 void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   //static char blink_count = 0;
-  //if (++blink_count == 125){
+  //if (++blink_count == 6){
     //turn_red();
+    //turn_green(); //remember to sound_on and change state amchine and comment thiss
     //turn_green();
-    //sound_on();
+    // sound_on();
     //blink_count = 0;
   //}
 
-  turn_red();
-  turn_green();
+  //turn_red();
+  // turn_green();
   sound_on();
+  
 }
