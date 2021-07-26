6d465302308d216d311594bde071cc9df93f1899
Osvaldo Nevarez <oanevarez@miners.utep.edu>
2021-07-26 17:02:19 -0600

    Update on State Machines

--- project/toy/stateMachines.c
+++ project/toy/stateMachines.c
@@ -6,35 +6,48 @@
 
 void turn_red() //button s1 turns on red led, and makes buzz noise
 {
-  if(switch_state_down_1){
     red_on=1;
     led_changed=1;
     led_update();
-  }else{
-    red_on=0;
-    led_changed=1;
-    led_update();
-  }
 }
 
 void turn_green() //button s2 turns on green led, and makes buzz noise
 {
-  if(switch_state_down_2){
-    green_on=1;
-    led_changed=1;
-    led_update();
-  }else{
-    green_on=0;
-    led_changed=1;
-    led_update();
-  }
+  green_on=1;
+  led_changed=1;
+  led_update();
 }
 
-void sound_on() //button s3 turn buzzer sound on to note C
-{
-  if(switch_state_down_3){
+void turn_greenOff(){
+  green_on =0;
+  led_changed =1;
+  led_update();
+}
+
+void turn_redOff(){
+  red_on =0;
+  led_changed =1;
+  led_update();
+}
+
+
+void sound_on(){
+  if(switch_state_down_1){
     buzzer_set_period(2109);
+    turn_red();
+  }else if(switch_state_down_2){
+    buzzer_set_period(1674);
+    turn_green();
+  }else if(switch_state_down_3){
+    buzzer_set_period(1408);
+    turn_green();
+  }else if(switch_state_down_4){
+    buzzer_set_period(2246);
+    turn_red();
   }else{
     buzzer_set_period(0);
+    turn_redOff();
+    turn_greenOff();
   }
+  //buzzer_set_period(0);
 }
